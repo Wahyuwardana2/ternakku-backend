8b8eb918cb5371cb2cb599354f2b52d6d9bd4f45
Wahyu Wardana <wahyu.wardana100@gmail.com>
2023-05-30 22:21:26 +0700

    Fix predicted_class different disease name

--- app.py
+++ app.py
@@ -7,5 +7,10 @@ app = Flask(__name__)
 app.register_blueprint(predictor_bp)
 app.register_blueprint(disease_details_bp)
 
+@app.route('/')
+def hello():
+    return "Sucess, ready to use"
+
+
 if __name__ == '__main__':
     app.run()
--- disease_details/details.py
+++ disease_details/details.py
@@ -2,13 +2,13 @@ def get_disease_details(predicted_class):
     if predicted_class == 'Cocciodiosis':
         disease_details = "Coccidiosis adalah penyakit parasit umum pada unggas yang disebabkan oleh protozoa dari genus Eimeria. Penyakit ini mempengaruhi saluran pencernaan dan dapat menyebabkan diare, penurunan berat badan, dan penurunan produksi telur."
         handling_method = "Untuk mengelola Coccidiosis, penting untuk menjaga kebersihan yang baik di tempat pemeliharaan unggas, menyediakan air minum yang bersih, dan menggunakan obat anticoccidial yang sesuai."
-    elif predicted_class == 'Ayam Sehat':
+    elif predicted_class == 'Healthy Chickens':
         disease_details = "Ayam Anda terlihat sehat."
         handling_method = "Terus berikan nutrisi yang tepat, air bersih, dan jaga kebersihan yang baik untuk memastikan kesehatan mereka."
-    elif predicted_class == 'Sapi Sehat':
+    elif predicted_class == 'Healthy Cows':
         disease_details = "Sapi Anda terlihat sehat."
         handling_method = "Terus berikan nutrisi yang tepat, air bersih, dan jaga kebersihan yang baik untuk memastikan kesehatan mereka."
-    elif predicted_class == 'Sapi Berbintik':
+    elif predicted_class == 'Lumpy Cows':
         disease_details = "Penyakit sapi berbintik, juga dikenal sebagai Penyakit Kulit Berbintik Bovine (LSD), adalah penyakit viral yang mempengaruhi sapi. Penyakit ini ditandai dengan demam, nodul kulit, dan pembengkakan."
         handling_method = "Untuk mengelola penyakit sapi berbintik, penting untuk mengisolasi hewan yang terinfeksi, menerapkan langkah pengendalian vektor, dan memberikan perawatan suportif. Konsultasikan dengan dokter hewan untuk opsi pengobatan yang spesifik."
     elif predicted_class == 'Salmonella':
